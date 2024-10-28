5de3528596a21028a07c7297a04fb0263553a275
leahjayholness <6leahjayholness@gmail.com>
2024-10-27 20:53:22 -0500

    added X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function(){
     let board = document.getElementById("board");
     for(const child of board.children){
         child.classList.add("square");
+        child.addEventListener('click', function(){
+            if(currentPlayer==1){
+                child.classList.add("X");
+                child.innerHTML = "X";
+            }
+            else{
+                child.classList.add("O");
+                child.innerHTML = "O";
+            }
+            currentPlayer *= -1;
+        })
     }
 })
 
+let currentPlayer = 1;
+let array1 = ['','','','','','','','',''];
